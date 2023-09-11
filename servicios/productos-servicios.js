f69c71b34cb04798e3e4a658a5e537e6d7fccf9a
NoeCarranza <123907943+NoeCarranza@users.noreply.github.com>
2023-09-10 21:23:07 -0300

    empezando dom

--- servicios/productos-servicios.js
+++ servicios/productos-servicios.js
@@ -1,11 +1,35 @@
-//GET
-
-const listaProductos = () => {
-    fetch('http://localhost:3000/producto')
-    .then(respuesta => respuesta.json())
-    .catch(error => console.log(error))
+// Función para crear una nueva línea de producto
+const crearNuevaLinea = (imageURL, name, price, id) => {
+    const linea = document.createElement("div");
+    linea.innerHTML = `
+        <img class="insumos__imagenes" src="${imageURL}">
+        <h1 class="insumos_descripcion">${name}</h1>
+        <h1 class="insumos__precio">$${price}</h1>
+        <a class="insumos__ver__producto" href="producto.html" id="${id}">
+            <h1 class="">Ver producto</h1>
+        </a>
+    `;
+    return linea; // Devuelve el elemento HTML creado
 }
 
-export const productoServicio = {
-    listaProductos
-}
+const table = document.querySelector("[data-product]");
+
+const http = new XMLHttpRequest();
+
+http.open("GET", "http://localhost:3000/producto");
+
+http.send();
+
+console.log(http);
+
+http.onload = () => {
+    const data = JSON.parse(http.response);
+    console.log(data);
+    data.forEach(product => {
+        const nuevaLinea = crearNuevaLinea(product.imageURL, product.name, product.price, product.id);
+        table.appendChild(nuevaLinea);
+    });
+};
+
+
+
